2758249ebe66310833a19f27f6b0ba1ddd6194bd
nghia0111 <20521659@gm.uit.edu.vn>
2023-07-03 23:42:55 +0700

    add firebase

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+  Filter,
+} = require("firebase-admin/firestore");
+require("dotenv").config();
 
 const cors = require("cors");
 app.use(cors());
@@ -9,19 +21,39 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-//console.log(io);
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_MAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
 
-app.get("/", function (req, res) {
-  res.send(`<h1>Hello</h1>`);
+initializeApp({
+  credential: cert(serviceAccount),
 });
 
+const db = getFirestore();
+
 io.on("connection", (socket) => {
-  console.log("connected");
-  socket.on("disconnect", (socket) => {
-    console.log("disconnected");
-    //  io.emit("remove-member", { member: member, room: room });
+  let userId = socket.handshake.query?.userId,
+    roomId = socket.handshake.query?.roomId;
+
+  socket.on("disconnect", async (socket) => {
+    try {
+      await db
+        .doc(`rooms/${roomId}/members/${userId}`)
+        .update({ isOnline: false });
+    } catch (err) {
+      console.log(err);
+    }
   });
-  socket.on("test", (mess) => {console.log(mess)})
 });
 
 server.listen(3000, () => {
